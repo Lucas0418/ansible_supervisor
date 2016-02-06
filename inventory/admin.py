8316455e0c4586305af69e841c7db667a51897eb
jslu <jslu0418@gmail.com>
2016-02-06 23:24:48 +0800

    Add group management of inventory

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,18 +1,34 @@
 from django.contrib import admin
-from .models import Host, Group, Var
+from .models import Host, Group, HostVar, GroupVar
 # Register your models here.
 
-class VarInline(admin.TabularInline):
-    model = Var
+
+class HostVarInline(admin.TabularInline):
+    model = HostVar
     extra = 1
+
 
 class HostAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,  {'fields': ['ansible_alias']}),
-        ('Data information',  {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
-    inlines = [VarInline]
+    inlines = [HostVarInline]
+
+
+class GroupVarInline(admin.TabularInline):
+    model = GroupVar
+    extra = 1
+
+
+class GroupAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,  {'fields': ['ansible_group']}),
+        ('ansible_hosts',  {'fields': ['ansible_hosts'], 'classes': ['collapse']}),
+        ('ansible_children',  {'fields': ['ansible_children'], 'classes': ['collapse']}),
+    ]
+    inlines = [GroupVarInline]
+    filter_horizontal = ('ansible_hosts', 'ansible_children')
+
 
 admin.site.register(Host, HostAdmin)
-admin.site.register(Group)
-#admin.site.register(Var)
+admin.site.register(Group, GroupAdmin)
